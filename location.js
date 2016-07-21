ed0d9306ad758dd3d5516e5dd3686d3cac3cdc77
Alexandru Zanfir <alexandru.zr@gmail.com>
2016-07-21 23:00:01 +0200

    change ip browser

--- location.js
+++ location.js
@@ -1,11 +1,11 @@
 var request = require('request');
-var url = 'http://ip-api.com/json/?fields=city';
-
+var url = 'http://ip-api.com/json/';
+var returnCity = '?fields=city';
 
 module.exports = function (ip) {
 	return new Promise(function (resolve, reject) {
 		request({
-			url: url ,
+			url: url + ip + returnCity ,
 			json: true
 		}, function (error, response, body) {
 			if (error) {
