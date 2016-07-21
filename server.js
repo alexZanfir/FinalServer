718719e7da45e232165ac64729ff11c683b4fa10
Alexandru Zanfir <alexandru.zr@gmail.com>
2016-07-21 22:12:39 +0200

    init rep

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+var middleware = require('./middleware.js');
+var location = require('./location.js');
+var weather = require('./weather.js');
+var a = location().then(function (loc) {
+	console.log(loc.city);
+		return (loc.city);
+	})
+var form = '<form><input name="q" placeholder="City name" /><button>Go</button></form>';
+
+
+app.use(middleware.logger);
+
+app.get('/about', function (req, res) {
+	var city = req.query.q;
+    console.log('city: ' +city);
+    if (city === undefined) {
+		location().then(function (loc) {
+			return weather(loc.city);
+		}).then(function (currentWeather) {
+			console.log(currentWeather);
+			res.send(currentWeather + form );
+		}).catch(function (error) {
+			console.log(error)
+		});
+	}else {
+		weather(city).then(function (currentWeather) {
+			console.log(currentWeather);
+			res.send(currentWeather + form );
+		}).catch(function (error) {
+			console.log(error)
+		});
+	}
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('Server started on port ' + PORT + '!');
+});
+
