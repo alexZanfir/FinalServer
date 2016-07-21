d17b7db7d434c40582c7fdbb1202edd86fa53bc9
Alexandru Zanfir <alexandru.zr@gmail.com>
2016-07-21 22:37:38 +0200

    change ip browser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var request = require('request');
 
 var middleware = require('./middleware.js');
 var location = require('./location.js');
