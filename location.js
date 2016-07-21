656c26d1b95f848dc514b83f0bf68c6dbfba21ad
Alexandru Zanfir <alexandru.zr@gmail.com>
2016-07-21 22:26:10 +0200

    change ip browser

--- location.js
+++ location.js
@@ -1,9 +1,9 @@
 var request = require('request');
 var url = 'http://ipinfo.io/';
 var ip = req.headers['x-forwarded-for'] || 
-     req.connection.remoteAddress || 
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress;
+     reqest.connection.remoteAddress || 
+     request.socket.remoteAddress ||
+     request.connection.socket.remoteAddress;
 
 module.exports = function () {
 	return new Promise(function (resolve, reject) {
